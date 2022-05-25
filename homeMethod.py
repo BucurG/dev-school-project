3111299b70d2ba5df65377226f0acc14c36fa265
Jianu <maria.jianu@ing.com>
2022-05-25 10:19:28 +0300

    changed home page

--- homeMethod.py
+++ homeMethod.py
@@ -1,8 +1,7 @@
-import json
 from flask import request, jsonify, Blueprint
 
 blueprintHome = Blueprint('homeMethod', __name__)
 
 @blueprintHome.route('/')
 def query_records():
-    return "Hello from your first Blueprint!"
+    return "Hello from the home page!"
